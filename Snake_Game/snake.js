c22f5da7c0b2a569749f4c1b8f4d12e0c186a3e0
DHANUSH <150118216+sdhanush-git@users.noreply.github.com>
2024-01-08 20:06:31 +0530

    Snake Game
    A Simple Snake game with Html, Css and JavaScript

---
+++ Snake_Game/snake.js
@@ -0,0 +1,130 @@
+const gameBoard = document.getElementById("gameBoard")
+const context = gameBoard.getContext('2d')
+const scoreText = document.getElementById('scoreVal')
+const WIDTH = gameBoard.width;
+const HEIGHT = gameBoard.height;
+const UNIT = 25;
+
+let foodX;
+let foodY;
+let xVel = 25;
+let yVel = 0;
+let score = 0;
+let active = true;
+let started = false;
+
+let snake = [
+        {x:UNIT*3,y:0},
+        {x:UNIT*2,y:0},
+        {x:UNIT,y:0},
+        {x:0,y:0}
+    ];
+window.addEventListener('keydown',keyPress)
+
+startGame();
+
+function startGame(){
+    context.fillStyle = 'black';
+    context.fillRect(0,0,WIDTH,HEIGHT);
+    createFood();
+    displayFood();
+    drawSnake();
+    
+}
+
+function clearBoard(){
+    context.fillStyle = 'black';
+    context.fillRect(0,0,WIDTH,HEIGHT);
+}
+function createFood(){
+    foodX = Math.floor(Math.random()*WIDTH / UNIT ) * UNIT
+    foodY = Math.floor(Math.random()*HEIGHT / UNIT ) * UNIT
+
+}
+function displayFood(){
+    context.fillStyle = 'green';
+    context.fillRect(foodX,foodY,UNIT,UNIT)
+}
+
+function drawSnake(){
+    context.fillStyle = 'red';
+    context.strokeStyle = 'black';
+    snake.forEach((snakePart)=>{
+        context.fillRect(snakePart.x,snakePart.y,UNIT,UNIT)
+        context.strokeRect(snakePart.x,snakePart.y,UNIT,UNIT)
+    }) 
+}
+function moveSnake(){
+    const head = {x:snake[0].x+xVel,
+                 y:snake[0].y+yVel}
+    snake.unshift(head) 
+    if(snake[0].x==foodX && snake[0].y==foodY){
+        createFood();
+        score += 1;
+        scoreText.textContent = score ;
+    }
+    else
+        snake.pop() 
+               
+}
+
+function nextTick(){
+    if(active){
+        setTimeout(()=>{
+            clearBoard();
+            displayFood();
+            moveSnake();
+            drawSnake();
+            nextTick();
+            checkGameOver();
+    },200);
+}
+else{
+    clearBoard();
+    context.font = "bold 50px serif";
+    context.fillStyle = "white";
+    context.textAlign = "center";
+    context.fillText("GAME OVER!!",WIDTH/2,HEIGHT/2)
+}
+}
+function keyPress(event){
+    if(!started){
+        started = true;
+        nextTick();
+    }
+    const LEFT = 37 
+    const UP = 38 
+    const RIGHT = 39 
+    const DOWN = 40
+    switch(true){
+        case(event.keyCode==LEFT && xVel!=UNIT):
+        xVel=-UNIT;
+        yVel=0;
+        break;
+        case(event.keyCode==RIGHT && xVel!=-UNIT):
+        xVel=UNIT;
+        yVel=0;
+        break;
+        case(event.keyCode==UP && yVel!=UNIT):
+        xVel=0;
+        yVel=-UNIT;
+        break;
+        case(event.keyCode==DOWN && yVel!=-UNIT):
+        xVel=0;
+        yVel=UNIT;
+        break;
+    }
+}
+
+function checkGameOver(){
+    switch(true){
+        case(snake[0].x<0) :      
+        case(snake[0].x>=WIDTH) :      
+        case(snake[0].y<0) :     
+        case(snake[0].y>=HEIGHT) :       
+
+        active=false;
+        break;
+    }
+}
+
